8f03b24720ee1d97f17fc6a33ba240f4555c7773
alldiney-mr <alldiney.mr@gmail.com>
2021-12-22 17:54:01 -0300

    Mobile Menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 import ScrollAnimate from "./modules/scroll-animate.js";
+import MobileMenu from "./modules/mobile-menu.js";
 
 const scrollAnimate = new ScrollAnimate('[data-animate="scroll"]');
 scrollAnimate.init();
+
+const mobileMenu = new MobileMenu('[data-menu="hamburguer"]', '[data-menu="lista"]');
+mobileMenu.init();
